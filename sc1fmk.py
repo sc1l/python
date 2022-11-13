5cc2e95f10743798ad72381a05683d002d3c112e
sjchoi10000 <77144160+sjchoi10000@users.noreply.github.com>
2022-11-13 10:47:56 +0900

    Update sc1fmk.py

--- sc1fmk.py
+++ sc1fmk.py
@@ -1,15 +1,20 @@
 #TMI : 파일 다시만듬
 
+print("need bs4 and os")
 from bs4 import BeautifulSoup
 import os
 from urllib.request import urlopen
 #load modules
 
+print("Python file loaded")
+
 if os.path.isdir("sc1fmk") == False :
     os.mkdir("sc1fmk")
 #make sc1fmk
 
-url = input("makebomb or hack or filebomb or Luck or stopwatch or class or sc1fmk : ")
+gitlink = urlopen("https://raw.githubusercontent.com/sjchoi10000/python/main/files")
+select = BeautifulSoup(gitlink.read(), "html.parser")
+url = input(select)
 #make input py file
 
 reset = open("sc1fmk/"+url+"from github.py", 'w')
@@ -28,3 +33,11 @@ raw = BeautifulSoup(giturl.read(), "html.parser")
 f.write(str(raw))
 f.close
 #write raw
+
+print("downloaded")
+yn = input("you want to view source? (Y/N)")
+if yn == "y" :
+    print(raw)
+    else :
+        if yn == "Y"
+            print(raw)
