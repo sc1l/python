d6f752aa962a382621d1ce0d5d9cd2456606f435
sjchoi10000 <77144160+sc1l@users.noreply.github.com>
2023-09-24 11:45:39 +0900

    Update stopwatch.py
    upgraded

--- stopwatch.py
+++ stopwatch.py
@@ -1,16 +1,20 @@
 import time
 
+nano = 00
 s = 00
 m = 00
 h = 00
 
 while True :
-    s += 1
+    nano += 1
+    if nano => 100 :
+        nano = 0
+        s += 1
     if s >= 60 :
         m += 1
         s = 0
     if m >= 60 :
         h += 1
         m = 0
-    print(s, "sec", m, "mintue", h, "hours")
-    time.sleep(1.000000)
+    print(h, ":", m, ":", s, ".", nano)
+    time.sleep(0.01)
